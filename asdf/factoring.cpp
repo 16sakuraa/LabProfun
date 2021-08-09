fd381c33e5b52a071e928b73ace4363e81478596
OAT <64010860@kmitl.ac.th>
2021-08-09 15:08:42 +0700

    added factoring

---
+++ asdf/factoring.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+int main()
+{
+	int num, i = 2;
+
+	printf("Enter Number : ");
+	scanf_s("%d", &num);
+	printf("Factoring Result : ");
+
+	while (num != 1)
+	{
+		if ((num % i) == 0)
+		{
+			num /= i;
+			printf("%d", i);
+			if (num != 1)
+			{
+				printf(" x ");
+			}
+		}
+		else
+		{
+			i++;
+		}
+	}
+
+	return 0;
+
+}
