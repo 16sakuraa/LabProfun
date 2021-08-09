b791ff22712ae59de2e68080c7112d6fbe734932
OAT <64010860@kmitl.ac.th>
2021-08-09 14:25:58 +0700

    updated

--- asdf/1.cpp
+++ asdf/1.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 int main()
 {
-	int num;
-	scanf_s("%d", &num);
-	printf("%d", num);
+	int a, b;
+	scanf_s("%d %d", &a, &b);
+
+	printf("a + b = %d\n", a + b);
+	printf("a - b = %d\n", a - b);
+	printf("a x b = %d\n", a * b);
+	printf("a / b = %d\n", a / b);
+
+	return 0;
+
 }
